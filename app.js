063aa1b1b4ccdc713ec940ee4bbf146169e5b1ff
juyerina <j35148@gmail.com>
2016-09-01 16:37:18 +0900

    upload home router

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var http = require("http");
+var router = require("./router");
 
+var app = http.createServer(function(req, res){
+	console.log(req.url);
 
-http.createServer(function(request, response){
-	response.writeHead(200, {"Content-type":"text/plain"});
-	response.write("Hello world \n");
-	response.end();
+	router.home(req, res);
+	router.detail(req, res);
+
 }).listen(3030);
 
 console.log("서버가 구동 중입니다. localhost:3030");
